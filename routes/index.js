aa270d0d56c6588d4c1c63ee43630c3948cdf46d
ANIL KUMAR <anilkumarmr@protonmail.com>
2022-03-14 16:37:54 +0530

    check

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,8 @@
 var express = require("express");
 var router = express.Router();
-
+var app = express();
+var cors = require("cors");
+app.use(cors());
 /* GET home page. */
 router.get("/", function (req, res, next) {
   res.render("index", { title: "eXpense tracker" });
