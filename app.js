b7669a16fdfcba701f33e78c8252823bc0ed1bf8
ANIL KUMAR <anilkumarmr@protonmail.com>
2022-03-14 16:52:30 +0530

    test

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 var createError = require("http-errors");
+var cors = require("cors");
 var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
-var cors = require("cors");
 
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
